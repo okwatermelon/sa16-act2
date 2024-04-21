8bc7338badb1c34ff7d748ee424c90ed89af71c9
Anthony Ybarra <ovfan@outlook.com>
2024-04-20 20:47:15 -0500

    finished

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,14 @@ class PagesController < ApplicationController
     def home
         render :home
     end
+    def about
+        render :about
+    end
+    def work
+        @work_image = 'banana.jpg'
+        render :work
+    end
+    def contact
+        render :contact
+    end
 end
